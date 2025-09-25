2f4576c6dd41f5335985b0fd964bd59e265ec782
Alexandru Rădulescu <alex11amr110506@gmail.com>
2025-09-25 12:06:57 +0300

    addded depolyment functions

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv"
 import cors from "cors"
+import path from "path"
 dotenv.config()
 import cookieParser from "cookie-parser"
 
@@ -13,6 +14,8 @@ import { connectDb } from "./lib/db.js";
 const app=express();
 const PORT=process.env.PORT
 
+const __dirname=path.resolve()
+
 app.use(cors({
     origin:"http://localhost:5173",
     credentials:true //allow frontend to send the cookies
@@ -23,6 +26,13 @@ app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
 app.use("/api/chat",chatRoutes)
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname+"/../frontend/dist")))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname+"/../frontend/dist/index.html"))
+    })
+}
 
 app.listen(PORT,()=>{
     console.log(`listnening on port ${PORT}...`);
